0882de48caacd08c6394169a51c8e3e55515244e
dhanikshah <dhanikshah@users.noreply.github.com>
2016-01-21 01:52:15 -0800

    First version of ToDo App

---
+++ app/src/main/java/com/mytestapp/todoapp/EditItemActivity.java
@@ -0,0 +1,36 @@
+package com.mytestapp.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditItemActivity extends AppCompatActivity {
+    private int pos;
+    private final int RESULT_OK = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        String itemText = getIntent().getStringExtra("text");
+        pos = getIntent().getIntExtra("position", -2);
+        EditText etEditedText = (EditText) findViewById(R.id.etEditItem);
+        etEditedText.setText(itemText);
+        etEditedText.setSelection(etEditedText.getText().length());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onSaveItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etEditItem);
+        String itemText = etNewItem.getText().toString();
+        Intent data = new Intent();
+        data.putExtra("text", itemText.toString());
+        data.putExtra("position", pos);
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
